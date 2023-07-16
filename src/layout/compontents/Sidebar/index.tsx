2ee45e46131e6ff2d17e77c4c9f3c289642b4804
LiqunHu <huliquns@126.com>
2023-07-16 16:47:08 +0800

    fix

--- src/layout/compontents/Sidebar/index.tsx
+++ src/layout/compontents/Sidebar/index.tsx
@@ -1,6 +1,5 @@
 import './style.css'
 import { Menu } from 'antd'
-import type { MenuProps } from 'antd'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { AppState } from '@/store'
